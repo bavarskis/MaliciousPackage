ae274445727363cea79baa9545812ae6111ba62e
Aurimas <bavarskiss@gmail.com>
2023-10-16 17:13:28 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -14,9 +14,6 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "MaliciousPackage",
-            path: "./Binaries/MaliciousActor.xcframework"),
-        .binaryTarget(
-            name: "MaliciousPackage",
             url: "https://github.com/bavarskis/MaliciousPackage/blob/main/Binaries/MaliciousActor.xcframework.zip",
             checksum: "afe087f15b24fdde8d7c0bccdefec7d6c95ca1aec66b4ea2e61edfe4f77d038b")
     ]
